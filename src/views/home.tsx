b0a8bc72cd119a9c9764aa1633ad34381f0f8121
Stef-00012 <me@stefdp.com>
2026-07-28 16:22:18 +0200

    add horsering

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -8,6 +8,8 @@ import { socials } from "@/data/socials";
 import { Tooltip } from 'react-tooltip'
 import Image from "next/image";
 import Link from "next/link";
+import { HackclubWebring } from "@/components/hackclubWebring";
+import { HorseRing } from "@/components/horseRing";
 
 const spotifyDefaultMessage = "Not listening to anything";
 const vscodeDefaultMessage = "Not coding anything";
@@ -255,86 +257,9 @@ export default function Home({ loading, status, onekoVariantCredits }: Props) {
 				</div>
 			</div>
 
-			<div className="fixed bottom-5 left-1/2 -translate-x-1/2 flex">
-				<button
-					type="button"
-					className="mx-1 cursor-pointer"
-					onClick={async () => {
-						const members = await fetch("https://webring.hackclub.com/members.json").then(res => res.json()) as ({
-							member: string,
-							url: string
-						})[]
-						
-						const meURL = new URL(document.referrer || window.location.href).hostname.toLowerCase()
-						
-						let me = members.findIndex(member => new URL(member.url).hostname.toLowerCase() === meURL)
+			<HackclubWebring />
 
-						if (me === -1) me = 0
-
-						const previous = (me - 1 + members.length) % members.length
-
-						window.location.href = members[previous].url;
-					}}
-				>
-					<span className="icon-[tabler--arrow-narrow-left] size-10 align-middle" />
-				</button>
-				<Link href="https://webring.hackclub.com/" className="mx-1 hover:underline">
-					<Image
-						width={50}
-						height={50}
-						src="https://assets.hackclub.com/icon-rounded.svg"
-						alt="Hack Club Webring"
-					/>
-				</Link>
-				<button
-					type="button"
-					className="mx-1 cursor-pointer pt-2"
-					onClick={async () => {
-						const members = await fetch("https://webring.hackclub.com/members.json").then(res => res.json()) as ({
-							member: string,
-							url: string
-						})[]
-
-						const meURL = new URL(document.referrer || window.location.href).hostname.toLowerCase()
-
-						let me = members.findIndex(member => new URL(member.url).hostname.toLowerCase() === meURL)
-
-						if (me === -1) me = 0
-						
-						let randomIndex = Math.floor(Math.random() * members.length)
-						
-						while (randomIndex === me) {
-							randomIndex = Math.floor(Math.random() * members.length)
-						}
-
-						window.location.href = members[randomIndex].url;
-					}}
-				>
-					<span className="icon-[ion--dice] size-8" />
-				</button>
-				<button
-					type="button"
-					className="mx-1 cursor-pointer"
-					onClick={async () => {
-						const members = await fetch("https://webring.hackclub.com/members.json").then(res => res.json()) as ({
-							member: string,
-							url: string
-						})[]
-						
-						const meURL = new URL(document.referrer || window.location.href).hostname.toLowerCase()
-						
-						let me = members.findIndex(member => new URL(member.url).hostname.toLowerCase() === meURL)
-
-						if (me === -1) me = 0
-
-						const next = (me + 1) % members.length
-
-						window.location.href = members[next].url;
-					}}
-				>
-					<span className="icon-[tabler--arrow-narrow-right] size-10 align-middle" />
-				</button>
-			</div>
+			<HorseRing />
 
 			{onekoVariantCredits && (
 				<div className="fixed  bottom-5 right-5 text-end text-xs text-base-content/50 p-2">
